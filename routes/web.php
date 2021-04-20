3248a8f89db4f53414e979e13b7e3621842de46e
Noruzzaman Rubel <noruzzamanrubel@gmail.com>
2021-04-20 10:56:26 +0600

    laravel 8 boilerplate multi auth

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\MainAdminController;
+use App\Http\Controllers\MainUserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function () {
+    Route::get('/login', [AdminController::class, 'loginForm']);
+    Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
+});
+
+Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+    return view('admin.index');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    return view('user.index');
+})->name('dashboard');
+
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+Route::get('/admin/profile', [MainAdminController::class, 'AdminProfile'])->name('admin.profile');
+Route::get('/admin/profile/edit', [MainAdminController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
+Route::post('/admin/profile/store', [MainAdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+Route::get('/admin/password/edit', [MainAdminController::class, 'AdminPasswordEdit'])->name('admin.password.edit');
+Route::post('/admin/password/update', [MainAdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
+
+//User All Route
+Route::get('/user/logout', [MainUserController::class, 'Logout'])->name('user.logout');
+Route::get('/user/profile', [MainUserController::class, 'UserProfile'])->name('user.profile');
+Route::get('/user/profile/edit', [MainUserController::class, 'UserProfileEdit'])->name('profile.edit');
+Route::post('/user/profile/store', [MainUserController::class, 'UserProfileStore'])->name('profile.store');
+Route::get('/user/password/edit', [MainUserController::class, 'UserPasswordEdit'])->name('user.password.edit');
+Route::post('/user/password/update', [MainUserController::class, 'UserPasswordUpdate'])->name('password.update');
